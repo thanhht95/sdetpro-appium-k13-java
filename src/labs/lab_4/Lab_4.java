a1d66eb0bfcae49b47ccec68388c93814fed14f3
unknown <thanhthanh1695@gmail.com>
2024-01-24 22:14:19 +0700

    update lab 4 to use ArrayList instead of Array

--- src/labs/lab_4/Lab_4.java
+++ src/labs/lab_4/Lab_4.java
@@ -1,8 +1,6 @@
 package labs.lab_4;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Lab_4 {
     public static void main(String[] args) {
@@ -15,6 +13,7 @@ public class Lab_4 {
             scanner.next();
         }
         int expectedArrLength = scanner.nextInt();
+
         System.out.print("Input the maximum number of the random range: ");
         while (!scanner.hasNextInt()) {
             System.out.println("That's not a number!");
@@ -24,20 +23,15 @@ public class Lab_4 {
         int maxRange = scanner.nextInt();
 
         //Random the value
-        Random randomIntNumbers = new Random();
-        int[] intArr = new int[expectedArrLength];
-        int arrLength = intArr.length;
-        for (int randomIndex = 0; randomIndex < arrLength; randomIndex++) {
-            intArr[randomIndex] = randomIntNumbers.nextInt(maxRange);
-        }
+        List<Integer> intArr = randomArrayListValue(expectedArrLength,maxRange);
 
         boolean isContinueing = true;
         do {
             printMenu();
-            int userOption = getUserOption();
 
-            //1. Print all numbers
+            int userOption = getUserOption();
             switch (userOption) {
+                //1. Print all numbers
                 case 1: {
                     printAllNumbers(intArr);
                     break;
@@ -74,8 +68,16 @@ public class Lab_4 {
     }
 
 
+    public static List<Integer> randomArrayListValue (int expectedArrLength, int maxRange){
+        Random randomIntNumbers = new Random();
+        List<Integer> intArr = new ArrayList<>();
+        for (int indexToRandom = 0; indexToRandom < expectedArrLength; indexToRandom++) {
+            intArr.add(randomIntNumbers.nextInt(maxRange));
+        }
+        return intArr;
+    }
+
     public static void printMenu() {
-        //Show menu
         System.out.println("=====MENU======");
         System.out.println("1. Print all numbers");
         System.out.println("2. Print maximum value");
@@ -86,7 +88,6 @@ public class Lab_4 {
 
     public static int getUserOption() {
         Scanner scanner = new Scanner(System.in);
-        //Let the user select option
         System.out.println("Input a corresponding number to select the option: ");
         while (!scanner.hasNextInt()) {
             System.out.println("That's not a number!");
@@ -96,34 +97,34 @@ public class Lab_4 {
         return scanner.nextInt();
     }
 
-    public static void printAllNumbers (int [] intArr){
-        System.out.println("All numbers in the array here " + Arrays.toString(intArr));
+    public static void printAllNumbers (List<Integer> intArr){
+        System.out.println("All numbers in the array here " + intArr);
         System.out.println("\nWhat do you want next?");
     }
 
-    public static void printMaxNumber (int [] intArr){
-        int maxValue = intArr[0];
-        for (int index = 1; index < intArr.length; index++) {
-            if (maxValue < intArr[index]) {
-                maxValue = intArr[index];
+    public static void printMaxNumber (List<Integer> intArr){
+        int maxValue = intArr.get(0);
+        for (int index = 1; index < intArr.size(); index++) {
+            if (maxValue < intArr.get(index)) {
+                maxValue = intArr.get(index);
             }
         }
         System.out.println("Max value is " + maxValue);
         System.out.println("\nWhat's do you want next?");
     }
 
-    public static void printMinNumber (int [] intArr){
-        int minValue = intArr[0];
-        for (int index = 1; index < intArr.length; index++) {
-            if (minValue > intArr[index]) {
-                minValue = intArr[index];
+    public static void printMinNumber (List<Integer> intArr){
+        int minValue = intArr.get(0);
+        for (int index = 1; index < intArr.size(); index++) {
+            if (minValue > intArr.get(index)) {
+                minValue = intArr.get(index);
             }
         }
         System.out.println("Min value is " + minValue);
         System.out.println("\nWhat do you want next?");
     }
 
-    public static void searchNumber(int[] intArr, int maxRange){
+    public static void searchNumber(List<Integer> intArr, int maxRange){
         Scanner scanner = new Scanner(System.in);
         //Get user input the number to search
         System.out.println("Which number do you want to search for? ");
@@ -139,8 +140,8 @@ public class Lab_4 {
             System.out.println("\nWhat do you want next? ");
         } else {
             boolean hasResult = false;
-            for (int index = 0; index < intArr.length; index++) {
-                if (userSearchingNumber == intArr[index]) {
+            for (int index = 0; index < intArr.size(); index++) {
+                if (userSearchingNumber == intArr.get(index)) {
                     hasResult = true;
                     System.out.println("The search number " + userSearchingNumber + " is in the list and the position is " + (index + 1));
                     System.out.println("\nWhat do you want next? ");
